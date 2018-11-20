72285c44eac508492fb2ab16c671ed6ea46f1757
Christopher Leah <chrisjohnleah@gmail.com>
2018-11-20 16:02:43 +0000

    Enqueue scripts, styles and the admin page

--- admin/class-cloudflare-ip-geolocation-redirects-admin.php
+++ admin/class-cloudflare-ip-geolocation-redirects-admin.php
@@ -73,6 +73,7 @@ class Cloudflare_Ip_Geolocation_Redirects_Admin {
 		 * class.
 		 */
 
+//		wp_enqueue_style( 'v-animate-css', 'https://unpkg.com/vue2-animate/dist/vue2-animate.min.css', array(), '2', 'all' );
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/cloudflare-ip-geolocation-redirects-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -95,9 +96,41 @@ class Cloudflare_Ip_Geolocation_Redirects_Admin {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
+		//TODO: Change Vue CDN URL from dev script to producton script: https://cdn.jsdelivr.net/npm/vue
+  		//wp_enqueue_script( $this->plugin_name.'-vuejs', 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js', array(), '2.5.17', false );
+		wp_enqueue_script( $this->plugin_name.'-vuejs', 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.js', array(), '2.5.17', false );
+		//https://jofftiquez.github.io/v-animate-css/
+		wp_enqueue_script( 'v-animate-css', 'https://unpkg.com/v-animate-css/dist/v-animate-css.js', array($this->plugin_name.'-vuejs'), '1', false );
+		//https://github.com/sagalbot/vue-select
+        //wp_enqueue_script( 'vue-select', 'https://unpkg.com/vue-select@latest', array($this->plugin_name.'-vuejs'), '2.5.3', false );
+		wp_enqueue_script( 'vee-validate', 'https://cdn.jsdelivr.net/npm/vee-validate@latest/dist/vee-validate.js', array($this->plugin_name.'-vuejs'), '2.1.3', false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/cloudflare-ip-geolocation-redirects-admin.js', array(), $this->version, true );
+	}
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/cloudflare-ip-geolocation-redirects-admin.js', array( 'jquery' ), $this->version, false );
+	/**
+	 * Add an options page under the Tools submenu
+	 *
+	 * @since  1.0.0
+	 */
+	public function add_options_page() {
 
+		$this->plugin_screen_hook_suffix = add_options_page(
+			__( 'Cloudflare IP Geolocation Redirects Settings', 'cl-cf-ip-gl-r' ),
+			__( 'Cloudflare IP Geolocation Redirects', 'cl-cf-ip-gl-r' ),
+			'manage_options',
+			$this->plugin_name,
+			array( $this, 'display_options_page' )
+		);
+
+	}
+
+	/**
+	 * Render the options page for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function display_options_page() {
+		include_once 'partials/cloudflare-ip-geolocation-redirects-admin-display.php';
 	}
 
 }
