087725c64a7c1c6ef254b44297b9533a79a0860c
Christopher Leah <chrisjohnleah@gmail.com>
2018-11-20 16:00:28 +0000

    Add options page

--- includes/class-cloudflare-ip-geolocation-redirects.php
+++ includes/class-cloudflare-ip-geolocation-redirects.php
@@ -156,6 +156,8 @@ class Cloudflare_Ip_Geolocation_Redirects {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_options_page' );
+
 
 	}
 
